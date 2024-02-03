eeed81630cb311a1cbb8496fcab8665d57c1f5e6
Dav1d43 <158083435+Dav1d43@users.noreply.github.com>
2024-02-03 20:27:08 +0400

    კლასები (ატრიბუტები, კლასის მეთოდები, კონსტრუქტორები)

---
+++ Lesson4.py
@@ -0,0 +1,25 @@
+class Student:
+
+    def __init__(self, name, grade, age):
+        self.name = name
+        self._grade = grade
+        self.age = age
+
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}, Grade: {self._grade}"
+
+    @property
+    def is_passing(self):
+        return self._grade > 60
+
+    def grade(self, amount):
+        self._grade += amount
+
+
+student1 = Student(name="Selena Gomez", grade=70, age=20)
+print(student1)
+print({student1.is_passing})
+student1.grade(5)
+
+
+
